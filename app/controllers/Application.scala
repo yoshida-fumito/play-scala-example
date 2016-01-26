1ebf131bd9fa9f69eca0ed93e725e22733afa440
yoshida-fumito <yoshida@unike.co.jp>
2016-01-26 16:43:17 +0900

    re update #25
    ex fix #25

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("unike software research"))
+    Ok(views.html.index("unike software research."))
   }
 
 }
